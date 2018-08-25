e19bf3e480b84f5066ffbf42b8ebd8d5c943c9e8
Gustavo Meneghetti <gustavo-meneghetti@hotmail.com>
2018-08-25 08:52:39 -0300

    Atulizando codigo maquina Gustavo

--- src/main/java/br/com/anatomiaumc/AnatomiaUMC/models/Role.java
+++ src/main/java/br/com/anatomiaumc/AnatomiaUMC/models/Role.java
@@ -20,7 +20,31 @@ public class Role {
 	@Id
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long id;
-    private String name;
+    public Long getId() {
+		return id;
+	}
+
+	public void setId(Long id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public Set<UsuarioModel> getUsers() {
+		return users;
+	}
+
+	public void setUsers(Set<UsuarioModel> users) {
+		this.users = users;
+	}
+
+	private String name;
   
     @ManyToMany(mappedBy = "roles")
     private Set<UsuarioModel> users = new HashSet<>();
--- src/main/java/br/com/anatomiaumc/AnatomiaUMC/models/UsuarioModel.java
+++ src/main/java/br/com/anatomiaumc/AnatomiaUMC/models/UsuarioModel.java
@@ -30,6 +30,7 @@ public class UsuarioModel {
 	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	@Column(name = "IDUsuario")
 	private Long idUsuario;
+	
 	@Column(name = "Nome")
 	private String nome;
 	@Column(name = "Email")
@@ -38,10 +39,86 @@ public class UsuarioModel {
 	private String login;
 	@Column(name = "Senha")
 	private String senha;
+	public Long getIdUsuario() {
+		return idUsuario;
+	}
+
+	public void setIdUsuario(Long idUsuario) {
+		this.idUsuario = idUsuario;
+	}
+
+	public String getNome() {
+		return nome;
+	}
+
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+
+	public String getEmail() {
+		return email;
+	}
+
+	public void setEmail(String email) {
+		this.email = email;
+	}
+
+	public String getLogin() {
+		return login;
+	}
+
+	public void setLogin(String login) {
+		this.login = login;
+	}
+
+	public String getSenha() {
+		return senha;
+	}
+
+	public void setSenha(String senha) {
+		this.senha = senha;
+	}
+
+	public Boolean getStatus() {
+		return Status;
+	}
+
+	public void setStatus(Boolean status) {
+		Status = status;
+	}
+
+	public String getResetToken() {
+		return resetToken;
+	}
+
+	public void setResetToken(String resetToken) {
+		this.resetToken = resetToken;
+	}
+
+	public CursoModel getCursoUsuario() {
+		return CursoUsuario;
+	}
+
+	public void setCursoUsuario(CursoModel cursoUsuario) {
+		CursoUsuario = cursoUsuario;
+	}
+
+	public Set<Role> getRoles() {
+		return roles;
+	}
+
+	public void setRoles(Set<Role> roles) {
+		this.roles = roles;
+	}
+
 	@Column(name = "Status")
 	private Boolean Status;
+	
+	
 	@Column(name = "reset_token")
 	private String resetToken;
+	
+	
 	@OneToOne
 	@JoinColumn(name = "IDCurso")
 	private CursoModel CursoUsuario;
